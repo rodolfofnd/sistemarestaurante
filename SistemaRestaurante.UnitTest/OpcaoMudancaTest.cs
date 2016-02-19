d71d3df7cc8fc46c39e9118269e8c7aa5bde5489
rodolfoa <rodolfoa@infoserver.com.br>
2016-02-19 08:51:54 -0200

    sistema

--- SistemaRestaurante.UnitTest/OpcaoMudancaTest.cs
+++ SistemaRestaurante.UnitTest/OpcaoMudancaTest.cs
@@ -7,7 +7,7 @@ namespace SistemaRestaurante.UnitTest
     public class OpcaoMudancaTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IncluiOpcao()
         {
         }
     }
